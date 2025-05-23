8513e037868b6a334973c1517324be75ffba1287
duzyanov <duzyanov@ozon.ru>
2025-05-23 19:37:42 +0300

    test

--- cmd/sorkin_bot/local/main.go
+++ cmd/sorkin_bot/local/main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"sorkin_bot/internal"
 )
 
 func main() {
 	ctx := context.Background()
+	fmt.Println("")
 	log.Fatal(internal.NewApp(ctx).Run(ctx))
 }
